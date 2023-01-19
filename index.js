a720e85a2d30523e9a1f4eaeec57063f29f0c19a
Bibazavr <lebedevki@lad24.ru>
2023-01-19 10:04:42 +0300

    chore: version up + add prerelease branches + update deps

--- index.js
+++ index.js
@@ -1,104 +1,108 @@
 module.exports = {
-    "branches": [
-        "master"
+  "branches": [
+    "master",
+    'next',
+    'next-major',
+    {name: 'release', prerelease: true},
+    {name: 'develop', prerelease: true}
+  ],
+  "plugins": [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        "preset": "angular",
+        "releaseRules": [
+          {
+            "type": "docs",
+            "release": false
+          },
+          {
+            "type": "style",
+            "release": false
+          },
+          {
+            "type": "ci",
+            "release": false
+          },
+          {
+            "type": "debug",
+            "release": false
+          },
+          {
+            "type": "test",
+            "release": false
+          },
+          {
+            "type": "build",
+            "release": false
+          },
+          {
+            "type": "refactor",
+            "release": false
+          },
+          {
+            "type": "perf",
+            "release": "patch"
+          },
+          {
+            "type": "fix",
+            "release": "patch"
+          },
+          {
+            "type": "chore",
+            "release": "patch"
+          },
+          {
+            "type": "revert",
+            "release": "patch"
+          }
+        ],
+        "parserOpts": {
+          "noteKeywords": [
+            "BREAK",
+            "BREAKING CHANGES",
+            "BREAKING CHANGE"
+          ]
+        }
+      }
     ],
-    "plugins": [
-        [
-            "@semantic-release/commit-analyzer",
-            {
-                "preset": "angular",
-                "releaseRules": [
-                    {
-                        "type": "docs",
-                        "release": false
-                    },
-                    {
-                        "type": "style",
-                        "release": false
-                    },
-                    {
-                        "type": "ci",
-                        "release": false
-                    },
-                    {
-                        "type": "debug",
-                        "release": false
-                    },
-                    {
-                        "type": "test",
-                        "release": false
-                    },
-                    {
-                        "type": "build",
-                        "release": false
-                    },
-                    {
-                        "type": "refactor",
-                        "release": false
-                    },
-                    {
-                        "type": "perf",
-                        "release": "patch"
-                    },
-                    {
-                        "type": "fix",
-                        "release": "patch"
-                    },
-                    {
-                        "type": "chore",
-                        "release": "patch"
-                    },
-                    {
-                        "type": "revert",
-                        "release": "patch"
-                    }
-                ],
-                "parserOpts": {
-                    "noteKeywords": [
-                        "BREAK",
-                        "BREAKING CHANGES",
-                        "BREAKING CHANGE"
-                    ]
-                }
-            }
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "CHANGELOG.md"
+      }
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        "npmPublish": false
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        "assets": [
+          "package.json",
+          "packages/**/package.json",
+          "CHANGELOG.md",
+          ["ios/**/Info.plist", "!ios/Pods/**"],
+          ["ios/**/project.pbxproj", "!ios/Pods/**"],
+          "android/app/build.gradle"
         ],
-        "@semantic-release/release-notes-generator",
-        [
-            "@semantic-release/changelog",
-            {
-                "changelogFile": "CHANGELOG.md"
-            }
-        ],
-        [
-            "@semantic-release/npm",
-            {
-                "npmPublish": false
-            }
-        ],
-        [
-            "@semantic-release/git",
-            {
-                "assets": [
-                    "package.json",
-                    "packages/**/package.json",
-                    "CHANGELOG.md",
-                    ["ios/**/Info.plist", "!ios/Pods/**"],
-                    ["ios/**/project.pbxproj", "!ios/Pods/**"],
-                    "android/app/build.gradle"
-                ],
-                "message": "chore(release): ${nextRelease.version} - <%= new Date().toLocaleDateString('ru-RU', {year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' }) %> [skip ci]\n"
-            }
-        ],
-        [
-            "@semantic-release/gitlab",
-            {
-                "assets": [
-                    {
-                        "path": "CHANGELOG.md",
-                        "label": "Changelog"
-                    }
-                ]
-            }
+        "message": "chore(release): ${nextRelease.version} - <%= new Date().toLocaleDateString('ru-RU', {year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' }) %> [skip ci]\n"
+      }
+    ],
+    [
+      "@semantic-release/gitlab",
+      {
+        "assets": [
+          {
+            "path": "CHANGELOG.md",
+            "label": "Changelog"
+          }
         ]
+      }
     ]
+  ]
 }
